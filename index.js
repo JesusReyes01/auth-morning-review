862975551c10e1d30fb8eae4f6dd68f52a23a705
jesusreyes01 <jesusareyes01@gmail.com>
2020-10-07 12:30:34 -0500

    now complete

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ require('dotenv').config();
 const express = require('express')
 const session = require('express-session')//top level middle ware
 const massive = require('massive')
-const authCtrl = './authController'
+const authCtrl = require('./authController')
 const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
-
 const app = express();
+
 app.use(express.json())//top level middle ware
+
 app.use(session({
     resave: false,
     saveUninitialized: true,
-    secret: SESSION_SECRET
+    secret: SESSION_SECRET,
     cookie: {maxAge: 1000 * 60 * 60 * 24 * 365}
 }))
+
 
 massive({
     connectionString: CONNECTION_STRING,
@@ -28,3 +30,6 @@ app.post('/api/login', authCtrl.login)
 app.get('/api/logout', authCtrl.logout)
 
 app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`))
+
+
+
